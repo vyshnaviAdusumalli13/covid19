01da5ab1c1bf1d46bc2326e4f5c83c82e84ede47
vyshnavi <adusumallivyshnavi13@gmail.com>
2021-12-23 13:24:53 +0000

    covid19

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -4,15 +4,14 @@ import {Link} from 'react-router-dom'
 
 const NotFound = () => (
   <div className="not-found-component">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
-      alt="Not Found"
-    />
     <h1 className="mt-4 not-found">PAGE NOT FOUND</h1>
     <p className="not-found-solution">
-      we’re sorry, the page you requested could not be found, Please go back to
-      the homepage
+      we are sorry, the page you requested could not be found
     </p>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not-found-pic"
+    />
     <button type="button" className="not-found-button">
       <Link to="/">Home</Link>
     </button>
